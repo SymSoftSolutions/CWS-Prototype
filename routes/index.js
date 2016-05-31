f3875285d572ae6a82981f61149760cb04a5de08
Matt Clemens <matt.d.clemens@gmail.com>
2016-05-31 11:35:00 -0700

    refactor profile logic

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ var utils = require('../lib/utils');
 var config = require('../config');
 var dbUtils = require('../lib/dbUtils');
 var exports = module.exports = utils.requireDir(__dirname);
+
+
+var newProfileRoutes = require('./newprofile');
+
+
+
 // var middleware = require('../middleware');
 
 var passport = require('passport');
@@ -43,41 +49,12 @@ function createAllRoutes(router) {
         res.end(n + ' views')
     });
 
-    createNewProfiles(router);
+    newProfileRoutes.createNewProfiles(router);
 
 
 }
 
 
-/**
- * Adds routes for handling new user creation
- * @param router
- */
-function createNewProfiles(router) {
-
-    /**
-     * Present to the user a form for entering in basic user details
-     */
-    router.get('/newprofile', function newProfileForm(req, res, next) {
-
-        dbUtils.listTableColumns('users').then((data) => {
-            console.log(data);
-            res.render('forms/newprofile');
-        });
-    });
-
-
-    /**
-     * Take user details, and if all entered in properly save user to the database.
-     * If something goes wrong we redirect back to the forms to try again, but with a message to the user too.
-     */
-    router.post('/newprofile', function newProfileSave(req, res, next) {
-
-        dbUtils.listTableColumns('users').then((data) => {
-            res.render('501', {status: 501, url: req.url});
-        });
-    });
-}
 
 
 function redirectToProfile(req, res, next) {
