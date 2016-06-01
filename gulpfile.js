10d35af1e2df55b91f341b24005a84352270a391
Matt Clemens <matt.d.clemens@gmail.com>
2016-06-01 16:17:45 -0700

    keep watching tests on failure

--- gulpfile.js
+++ gulpfile.js
@@ -12,42 +12,54 @@ var frontendTests = 'test/*.e2e.test.js';
 gulp.task('default', ['browser-sync']);
 gulp.task('test', ['test-backend', 'watch-backend']);
 
-gulp.task('browser-sync', ['nodemon'], function() {
-  browserSync.init(null, {
-    proxy: "http://localhost:" + config.server.port,
-    files: ["views/**/*.*"],
-    browser: "google chrome",
-    port: 7000,
-  });
+gulp.task('browser-sync', ['nodemon'], function () {
+    browserSync.init(null, {
+        proxy: "http://localhost:" + config.server.port,
+        files: ["views/**/*.*"],
+        browser: "google chrome",
+        port: 7000,
+    });
 });
 
 gulp.task('nodemon', function (cb) {
-  var started = false;
+    var started = false;
 
-  return nodemon({
-    script: 'server.js',
-    ignore: ['frontend/', backendTests, 'node_modules/**/*.js']
-  }).on('start', function () {
-    if (!started) {
-      cb();
-      started = true;
+    return nodemon({
+        script: 'server.js',
+        ignore: ['frontend/', backendTests, 'node_modules/**/*.js']
+    }).on('start', function () {
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
+});
+
+var watching = false;
+
+function onError(err) {
+    console.log(err.toString());
+    if (watching) {
+        this.emit('end');
+    } else {
+        // if you want to be really specific
+        process.exit(1);
     }
-  });
-});
+}
+
+gulp.task('test-backend', function () {
+    gulp.src(backendTests)
+        .pipe(mocha({
+            reporter: 'spec',
+            istanbul: false,
+        })).on("error", onError)
+})
 
 
-gulp.task('test-backend', function(){
-  gulp.src(backendTests)
-      .pipe(mocha({
-        reporter: 'spec',
-        istanbul: false,
-      }));
-});
-
-
-var backendFiles = ['middleware/**/*.js',  'server.js', 'lib/**/*.js', backendTests, 'models/**/*.js'];
+var backendFiles = ['middleware/**/*.js', 'server.js', 'lib/**/*.js', backendTests, 'models/**/*.js'];
 
 // Watch Files For Changes
-gulp.task('watch-backend',  function() {
-  gulp.watch(backendFiles, ['test-backend']);
+gulp.task('watch-backend', function () {
+    watching = true;
+    gulp.watch(backendFiles, ['test-backend']);
 });
